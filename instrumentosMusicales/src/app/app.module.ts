a54d9aeeaf0c3bb4b617c238e46a90663739509d
jdmartinez859 <jdmartinez859@misena.edu.co>
2018-09-11 14:43:11 -0500

    fixed: se trabaja en el home

--- instrumentosMusicales/src/app/app.module.ts
+++ instrumentosMusicales/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { AppRoutingModule }  from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
